4a26af5cf283dbfdc9b6c540a9da7b457ec0bc60
bala-amypo <bala@amypo.in>
2025-12-17 05:17:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/LocationEntity.java
@@ -0,0 +1,12 @@
+package com.example.demo.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class LocationEntity{
+  @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  private 
+}
